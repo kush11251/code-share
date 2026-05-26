704097ec41df1b05849d4f24a6c7ec3a0902be92
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-05-27 00:45:00 +0530

    Code update
    1. Json handling
    2. Code highlighting

--- static/app.js
+++ static/app.js
@@ -6,6 +6,8 @@
 
   const elements = {
     editor: document.getElementById('editor'),
+    codeHighlight: document.getElementById('codeHighlight'),
+    codeDisplay: document.getElementById('codeDisplay'),
     language: document.getElementById('languageSelect'),
     statusText: document.getElementById('statusText'),
     connectionText: document.getElementById('connectionText'),
@@ -21,6 +23,95 @@
     clientToken: document.getElementById('clientToken'),
     toast: document.getElementById('toast'),
   };
+
+  function highlightCode(code, language) {
+    const keywords = {
+      javascript: ['function', 'const', 'let', 'var', 'if', 'else', 'for', 'while', 'return', 'class', 'import', 'export', 'async', 'await', 'try', 'catch', 'finally', 'new', 'this', 'super', 'static'],
+      python: ['def', 'class', 'if', 'else', 'elif', 'for', 'while', 'return', 'import', 'from', 'as', 'try', 'except', 'finally', 'with', 'lambda', 'yield', 'raise', 'assert', 'pass', 'break', 'continue', 'and', 'or', 'not', 'in', 'is'],
+      go: ['package', 'import', 'func', 'var', 'const', 'type', 'struct', 'interface', 'if', 'else', 'for', 'range', 'switch', 'case', 'default', 'return', 'defer', 'go', 'chan', 'select', 'map', 'make', 'new', 'len', 'cap', 'append', 'copy', 'delete'],
+      html: ['html', 'head', 'body', 'div', 'span', 'a', 'img', 'script', 'style', 'link', 'meta', 'title', 'h1', 'h2', 'h3', 'p', 'ul', 'li', 'form', 'input', 'button', 'table', 'tr', 'td', 'class', 'id'],
+      css: ['font-size', 'color', 'background', 'margin', 'padding', 'border', 'display', 'flex', 'grid', 'position', 'width', 'height', 'transform', 'transition', 'animation', 'box-shadow', 'border-radius', 'overflow', 'z-index'],
+      json: ['true', 'false', 'null']
+    };
+
+    const keywordSet = new Set(keywords[language] || []);
+    let highlighted = '';
+    let i = 0;
+
+    while (i < code.length) {
+      if (code[i] === '"' || code[i] === "'") {
+        const quote = code[i];
+        let str = quote;
+        i++;
+        while (i < code.length && code[i] !== quote) {
+          if (code[i] === '\\') {
+            str += code[i] + code[i + 1];
+            i += 2;
+          } else {
+            str += code[i];
+            i++;
+          }
+        }
+        if (i < code.length) str += code[i++];
+        highlighted += '<span class="string">' + escapeHtml(str) + '</span>';
+      } else if (code[i] === '/' && code[i + 1] === '/' && (language === 'javascript' || language === 'go' || language === 'css')) {
+        let comment = '';
+        while (i < code.length && code[i] !== '\n') {
+          comment += code[i++];
+        }
+        highlighted += '<span class="comment">' + escapeHtml(comment) + '</span>';
+      } else if (code[i] === '#' && (language === 'python' || language === 'html')) {
+        let comment = '';
+        while (i < code.length && code[i] !== '\n') {
+          comment += code[i++];
+        }
+        highlighted += '<span class="comment">' + escapeHtml(comment) + '</span>';
+      } else if (/\d/.test(code[i])) {
+        let num = '';
+        while (i < code.length && /[\d.]/.test(code[i])) {
+          num += code[i++];
+        }
+        highlighted += '<span class="number">' + num + '</span>';
+      } else if (/[a-zA-Z_]/.test(code[i])) {
+        let word = '';
+        while (i < code.length && /[a-zA-Z0-9_]/.test(code[i])) {
+          word += code[i++];
+        }
+        if (keywordSet.has(word)) {
+          highlighted += '<span class="keyword">' + word + '</span>';
+        } else if (language === 'html' && word.match(/^[A-Z]/)) {
+          highlighted += '<span class="tag">' + word + '</span>';
+        } else {
+          highlighted += word;
+        }
+      } else if (code[i] === '<' && language === 'html') {
+        let tag = '';
+        while (i < code.length && code[i] !== '>') {
+          tag += code[i++];
+        }
+        if (i < code.length) tag += code[i++];
+        highlighted += '<span class="tag">' + escapeHtml(tag) + '</span>';
+      } else {
+        highlighted += escapeHtml(code[i]);
+        i++;
+      }
+    }
+
+    return highlighted;
+  }
+
+  function escapeHtml(text) {
+    const map = { '&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;', "'": '&#39;' };
+    return text.replace(/[&<>"']/g, m => map[m]);
+  }
+
+  function updateCodeDisplay() {
+    const code = elements.editor.value;
+    const language = elements.language.value;
+    elements.codeHighlight.innerHTML = highlightCode(code, language);
+    elements.codeDisplay.scrollTop = elements.editor.scrollTop;
+    elements.codeDisplay.scrollLeft = elements.editor.scrollLeft;
+  }
 
   function updateStatus() {
     const isConnected = ws && ws.readyState === WebSocket.OPEN;
@@ -87,12 +178,14 @@
           lockHolder = msg.holder || null;
           if (typeof msg.code === 'string') {
             elements.editor.value = msg.code;
+            updateCodeDisplay();
           }
           setReadOnly(lockHolder !== clientToken);
         }
         if (msg.type === 'code_update') {
           if (typeof msg.code === 'string') {
             elements.editor.value = msg.code;
+            updateCodeDisplay();
           }
         }
         if (msg.type === 'lock_status' || msg.type === 'lock_granted') {
@@ -181,13 +274,22 @@
     showToast('Reconnecting WebSocket...', 'success');
   });
   elements.themeBtn.addEventListener('click', toggleTheme);
+  elements.language.addEventListener('change', () => {
+    updateCodeDisplay();
+  });
   elements.editor.addEventListener('input', () => {
     updateStatus();
+    updateCodeDisplay();
     clearTimeout(codeUpdateTimer);
     codeUpdateTimer = setTimeout(sendCodeUpdate, 250);
+  });
+  elements.editor.addEventListener('scroll', () => {
+    elements.codeDisplay.scrollTop = elements.editor.scrollTop;
+    elements.codeDisplay.scrollLeft = elements.editor.scrollLeft;
   });
 
   setReadOnly(true);
   updateStatus();
+  updateCodeDisplay();
   connectWebSocket();
 })();
